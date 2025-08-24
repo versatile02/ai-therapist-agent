2f6ce5504d13e96777b80c7c45221f2f66dae952
TABISH IQBAL <146206975+Tabish166@users.noreply.github.com>
2025-08-24 15:03:50 +0530

    Delete node_modules directory

--- node_modules/@inngest/ai/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export type { AiAdapter, AiAdapters } from "./adapter.js";
-export * from "./adapters/index.js";
-export * from "./models/index.js";
-export * as models from "./models/index.js";
-//# sourceMappingURL=index.d.ts.map
